2bc0573cbdc1b2e98e64174941af22eeff374b1a
Geoff <gwebb762@gmail.com>
2017-11-11 11:19:36 -0800

    Added models to AppDbContext

--- LBMCjamz/Models/AppDbContext.cs
+++ LBMCjamz/Models/AppDbContext.cs
@@ -9,13 +9,11 @@ namespace LBMCjamz.Models
     public class AppDbContext : DbContext
     {
         public AppDbContext() : base("LBMCjamz")
-        {
+        {}
 
-        }
-
-        //public DbSet<Artist> Artists { get; set; }
-        //public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
         //public DbSet<Song> Songs { get; set; }
-   
+
     }
 }
