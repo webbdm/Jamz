ba4936facc20d7117eaee709f8f00bc76a6e894e
Geoff <gwebb762@gmail.com>
2017-11-10 20:06:31 -0800

    Setup Basic Styles on ng-Views

--- LBMCjamz/ngApp/Controllers/jamzController.js
+++ LBMCjamz/ngApp/Controllers/jamzController.js
@@ -1,8 +1,19 @@
-app.controller("jamzController",["$scope","$http",function($scope,$http) {
+app.controller("jamzController", ["$scope", "$http", function ($scope, $http) {
 
-   /* $http.get("/api/values").then(function(result) {
-        $scope.values = result.data;
-    });*/
+    /* $http.get("/api/values").then(function(result) {
+         $scope.values = result.data;
+     });*/
+
+    let artistList = [
+        {
+            name: 'Aerosmith'
+        },
+        {
+            name: 'Pearl Jam'
+        }
+    ];
+
+    $scope.artists = artistList;
 
     $scope.message = "Jamz App!";
     console.log($scope.message);
